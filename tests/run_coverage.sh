c032902459ae3035a10a9a9db505250434809fdf
Vladimir Roncevic <elektron.ronca@gmail.com>
2025-11-26 21:34:19 +0100

    [gen_mmap] Updated code coverage, version, docs

--- tests/run_coverage.sh
+++ tests/run_coverage.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 #
 # @brief   gen_mmap
-# @version v1.0.3
+# @version v1.0.4
 # @date    Sat Aug 11 09:58:41 2018
 # @company None, free software to use 2018
 # @author  Vladimir Roncevic <elektron.ronca@gmail.com>
@@ -9,10 +9,11 @@
 
 rm -rf htmlcov gen_mmap_coverage.xml gen_mmap_coverage.json .coverage
 rm -rf fresh_new/ new_simple_test/ full_simple/ full_simple_new/ latest_pro/
-ats_coverage_run.py -n gen_mmap -p ../README.md
-rm -rf fresh_new/ new_simple_test/ full_simple/ full_simple_new/ latest_pro/
 python3 -m coverage run -m --source=../gen_mmap unittest discover -s ./ -p '*_test.py' -vvv
 python3 -m coverage html -d htmlcov
 python3 -m coverage xml -o gen_mmap_coverage.xml 
 python3 -m coverage json -o gen_mmap_coverage.json
 python3 -m coverage report --format=markdown -m
+python3 ats_coverage.py -n gen_mmap
+rm htmlcov/.gitignore
+echo "Done"
